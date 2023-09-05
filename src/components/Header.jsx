d3c5c5ee39d6b46be997fe25bd89e04a412506d5
Andrey <ilynych0810@gmail.com>
2023-09-05 12:46:44 +0200

    Header, Nav, Content

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-256.png'/>
+        </header>
+    );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Nav = () => {
+    return (
+        <nav className='nav'>
+            <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Messages</a></li>
+            <li><a href='#'>News</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Contacts</a></li>
+            </ul>
+        </nav>
+    );
+}
+
