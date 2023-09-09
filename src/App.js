ed87f5365538b3461549dfe97c730c6115e8ab53
Andrey <ilynych0810@gmail.com>
2023-09-09 06:53:40 +0200

    Adding Router-dom / Adding some styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Nav } from './components/Nav/Nav';
+
 import { Profile } from './components/Profile/Profile';
+import { Dialogs } from './components/Dialogs/Dialogs';
+import { News } from './components/News/News';
+import { Music } from './components/Music/Music';
+import { Contacts } from './components/Contacts/Contacts';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header/>
+        <Nav />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/contacts' elementm={<Contacts />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from './Nav.module.css';
+import {NavLink} from 'react-router-dom';
 
 export const Nav = () => {
     return (
         <nav className={s.nav}>
             <ul>
-            <li><a className={s.active} href='#'>Home</a></li>
-            <li><a href='#'>Messages</a></li>
-            <li><a href='#'>News</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Contacts</a></li>
+                <li><NavLink to='/profile' activeClassName={s.activeLink}>Home</NavLink></li>
+                <li><NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink></li>
+                <li><NavLink to='/news' activeClassName={s.activeLink}>News</NavLink></li>
+                <li><NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink></li>
+                <li><NavLink to='/contacts' activeClassName={s.activeLink}>Contacts</NavLink></li>
             </ul>
         </nav>
     );
