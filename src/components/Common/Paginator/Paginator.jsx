4e5752e689e13c8a0aa6702e5208616759ed5ac7
Andrey <ilynych0810@gmail.com>
2024-02-03 09:22:32 +0100

    Paginator, TDD, (the beginning of familiarization with Classnames)

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import s from './Paginator.module.css';
+import cn from 'classnames';
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChange}) => {
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+const Paginator = ({totalItemsCount, pageSize, currentPage, onPageChange, portionSize = 10}) => {
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
     
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    
+
+    // Кіль-сть порцій 
+    let portionCount = Math.ceil(pagesCount / pageSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    // Щоб узнати який номер лівої границі в кожній порції, ми викор-мо формулу: (p - 1) * ps + 1.
+    // p(portion) -- це номер порції; ps(portionSize) - це кількість юзерів в сторінці;
+    // (Необов'язковий) + 1 - це коли номерування хочемо почати з 1, а не з 0
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    // Тут нам показує праву сторону, тобто кінець кожної порції. У нас вийде 10 (кінець першої порції)
+    let rightPortionPageNumber = portionNumber * portionSize;
+
     return <div className={s.pagination}>
-        {pages.map(p => {
-            return <span className={currentPage === p && s.active} onClick={(e) => {onPageChange(p)}}>{p}</span>
-        })}
+        {portionNumber > 1 && 
+        <button onClick={() => { setPortionNumber(portionNumber - 1)}}>Back</button>}
+
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
+                return ( 
+                <span 
+                    className={ cn({
+                        [s.selectedPage]: currentPage === p
+                    }, [s.pageNumber]) } 
+                    key={p} 
+                    onClick={(e) => {onPageChange(p)}}>{p}
+                </span>)
+            })
+        }
+        {portionCount > portionNumber &&
+            <button onClick={() => { setPortionNumber(portionNumber + 1) }}> Next </button>}
     </div>
 }
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import s from './Users.module.css';
-import userPhoto from '../../assets/img/user.png';
-import {Navigate, NavLink} from 'react-router-dom';
 import Paginator from "../Common/Paginator/Paginator";
 import User from "./User";
 
 const Users = ({totalUsersCount, pageSize, currentPage, onPageChange, users, ...props}) => {
     return (
         <div className={s.wrapper}>
-            <Paginator totalUsersCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage} onPageChange={onPageChange} />
+            <Paginator totalItemsCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage} onPageChange={onPageChange} />
             <div>
                 {users.map(u => <User user={u} followingInProgress={props.followingInProgress} follow={props.follow} unfollow={props.unfollow} key={u.id} {...props} />)}
             </div>
